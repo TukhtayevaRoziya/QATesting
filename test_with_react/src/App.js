719af1ab9f3d2cfa107f1156b7becb2202471839
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2022-07-20 11:27:44 +0500

    some fix

--- test_with_react/src/App.js
+++ test_with_react/src/App.js
@@ -1,9 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
+  const [data, setData] = useState(null);
+  const [toggle, setToggle] = useState(false);
+
+  const onClick =()=> setToggle(prev=> !prev)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData({})
+    }, 1000);
+  }, []);
+
+
   return (
     <main>
+      {data && <div style={{color: "red"}}>data</div>}
     Hello
+    <button onClick={onClick}>send</button>
+    <input placeholder='Inp Text'/>
     </main>
   );
 };
--- test_with_react/src/__tests__/App.test.js
+++ test_with_react/src/__tests__/App.test.js
@@ -1,12 +1,20 @@
+import '@testing-library/jest-dom'
+
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '../App';
 
 describe('<App />', () => {
-  it('renders the menu with two burriots & one already in the basket', () => {
-    const {  getByText } = render(
-      <App />
-    );
-    expect(getByText('Hello')).toBeTruthy();
+  it('renders the menu with two burriots & one already in the basket', async() => {
+  render(<App />);
+    screen.debug()
+    const find = await screen.findByText(/data/i)
+    const placeholder = await screen.getByPlaceholderText(/Inp Text/i)
+    expect(find).toBeInTheDocument()
+    expect(find).toHaveStyle({color: 'red'})
+    expect(placeholder).toBeInTheDocument()
+    screen.debug()
+
+    // expect(getByRole('button')).toBeTruthy();
   });
 });
