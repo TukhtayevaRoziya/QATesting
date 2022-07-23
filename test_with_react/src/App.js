a6545bb205fea9c2065c449299c9ca547bd44ca6
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2022-07-23 22:52:24 +0500

    fix

--- test_with_react/src/App.js
+++ test_with_react/src/App.js
@@ -2,16 +2,18 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [toggle, setToggle] = useState(false);
+  const [value, setValue] = useState('');
 
   const onClick =()=> setToggle(prev=> !prev)
 
 
   return (
     <main>
+      <h1 data-testid={'value-elem'}>{test}</h1>
       {toggle === true && <div data-testid={'toggle-elem'} style={{color: "red"}}>toggle</div>}
     Hello
     <button data-testid={'toggle-btn'} onClick={onClick}>send</button>
-    <input placeholder='Inp Text'/>
+    <input onChange={e=>setValue(e.target.value)} placeholder='Inp Text'/>
     </main>
   );
 };
--- test_with_react/src/__tests__/App.test.js
+++ test_with_react/src/__tests__/App.test.js
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom'
 
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 describe('<App />', () => {
@@ -17,10 +18,28 @@ describe('<App />', () => {
   // });
   test('click event', () => {
     render(<App />);
-    const btn = screen.getByTestId('toggle-btn')
-    const elem = screen.queryByTestId('toggle-elem')
-    expect(elem).toBeNull()
-    fireEvent.click(btn)
-    expect(elem).toBeInTheDocument()
+    // const btn = screen.getByTestId('toggle-btn')
+    // const elem = screen.queryByTestId('toggle-elem')
+    // expect(elem).toBeNull()
+    // fireEvent.click(btn)
+    // expect(elem).toBeInTheDocument()
+  });
+  test('click event', () => {
+    render(<App />);
+        const placeholder = screen.getByPlaceholderText(/Inp Text/i)
+    expect(screen.queryByTestId('value-elem')).toContain('')
+    // fireEvent.input(placeholder,{
+    //   target: {value: 'sdfsdf'}
+    // })
+    userEvent.type(placeholder, 'sdfsdf')
+    expect(screen.queryByTestId('value-elem')).toContain('sdfsdf')
+
+
+
+    // const btn = screen.getByTestId('toggle-btn')
+    // const elem = screen.queryByTestId('toggle-elem')
+    // expect(elem).toBeNull()
+    // fireEvent.click(btn)
+    // expect(elem).toBeInTheDocument()
   });
 });
